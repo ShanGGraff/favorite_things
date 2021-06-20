abcb58a9a61eb6a7b8fb6afed88e569f6f0951d5
Shane Graff <copellius@gmail.com>
2021-06-20 11:30:03 -0700

    Add array and output
    Co-authored-by: Ryan Rendon <rendon.s.ryan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,20 @@
   
 //UserInterface Logic
 $(document).ready(function() {
-  $("form#").submit(function(event) {
-    
+  $("form#info").submit(function(event) {
+    const question1 = $("#question1").val();
+    const question2 = $("#question2").val();
+    const question3 = $("#question3").val();
+    const question4 = $("#question4").val();
 
+    const usedThings = [];
+    const favoriteThings = [question1, question2, question3, question4];
+    usedThings.push(question1, question2, question3);
+
+    $(".drink").text(usedThings[1]);
+    $(".color").text(usedThings[0]);
+    $(".spice").text(usedThings[2]);
+    $(".answers").show();
 
     event.preventDefault();
   });
